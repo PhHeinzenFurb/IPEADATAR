3000a5190fcdee46d972b58c5fcff5a74e4ccfc7
PhHeinzenFurb <phheinzen@furb.br>
2025-07-12 18:02:57 -0300

    Atualizado ipeadata_caged.R
    Feita a extração e manipulação dos dados do CAGED para admissões e demissões no Brasil, separados por município

--- ipeadata_caged.R
+++ ipeadata_caged.R
@@ -1,11 +1,26 @@
 library(ipeadatar)
 library(tidyverse)
+library(data.table)
 
+# verificando as séries dentro de ipeadataR
 series_ipeadata <- ipeadatar::available_series() 
 
+# selecionando os municipios dentro dos territorios disponiveis
 territories_ipeadata <- ipeadatar::available_territories() %>%
   filter(uname == "Municipality") %>%
   select(tcode, tname)
 
-dados_ipeadata <- ipeadatar::ipeadata("ADMISNC") %>%
+codigos <- c("Admissões" = "ADMISNC", "Demissões" = "DESLIGNC")
+
+# dados de admissoes da IPEA, filtrados por municipios
+dados_caged <- ipeadatar::ipeadata(codigos) %>%
   filter(uname == "Municipality")
+
+dados_caged_trans <- dados_caged %>%
+  mutate(categoria = case_when(
+    str_detect(code, "ADMISNC") ~ "Admissões",
+    str_detect(code, "DESLIGNC") ~ "Demissões"
+  ),
+  tcode = as.character(tcode)) %>%
+  left_join(territories_ipeadata, by = "tcode") %>%
+  select(date, value, categoria, tname)
