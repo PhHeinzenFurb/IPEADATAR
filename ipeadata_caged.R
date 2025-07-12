878a3509cce622e91ad37afc5f4871959ea9e5c9
PhHeinzenFurb <phheinzen@furb.br>
2025-07-12 20:45:38 -0300

    Atualizado ipeadata_caged.R
    Atualizado com a criação de um leaflet com os dados de admissão da tabela dados_caged_sf

--- ipeadata_caged.R
+++ ipeadata_caged.R
@@ -1,26 +1,58 @@
+# chamando bibliotecas
 library(ipeadatar)
 library(tidyverse)
-library(data.table)
+library(geobr)
+library(leaflet)
+library(sf)
 
-# verificando as séries dentro de ipeadataR
-series_ipeadata <- ipeadatar::available_series() 
+# extraindo dados geoespaciais dos municipios
+municipios <- read_municipality(year = 2022, simplified = T)
 
-# selecionando os municipios dentro dos territorios disponiveis
-territories_ipeadata <- ipeadatar::available_territories() %>%
-  filter(uname == "Municipality") %>%
-  select(tcode, tname)
-
+# codigos a serem utilizados na extracao
 codigos <- c("Admissões" = "ADMISNC", "Demissões" = "DESLIGNC")
 
 # dados de admissoes da IPEA, filtrados por municipios
 dados_caged <- ipeadatar::ipeadata(codigos) %>%
-  filter(uname == "Municipality")
-
-dados_caged_trans <- dados_caged %>%
+  filter(uname == "Municipality") %>%
   mutate(categoria = case_when(
     str_detect(code, "ADMISNC") ~ "Admissões",
     str_detect(code, "DESLIGNC") ~ "Demissões"
-  ),
-  tcode = as.character(tcode)) %>%
-  left_join(territories_ipeadata, by = "tcode") %>%
-  select(date, value, categoria, tname)
+  )) %>%
+  left_join(municipios, by = c("tcode" = "code_muni")) %>%
+  select(-c(code, tcode, code_state, code_region)) %>%
+  pivot_wider(
+    names_from = categoria,
+    values_from = value
+  )
+
+dados_caged_sf <- dados_caged %>%
+  st_as_sf() %>%
+  st_transform(crs = 4326)
+
+m <- dados_caged_sf %>%
+  filter(date == "2025-01-01") %>%
+  leaflet() %>%
+  addTiles() %>%
+  addPolygons(stroke = T,
+              color = "#2C2D2D",
+              weight = 1.0,
+              fillOpacity = 0.5, 
+              smoothFactor = 1,
+              fillColor = ~ colorQuantile(
+                palette = "YlOrRd",
+                domain = Admissões,
+                na.color = "transparent",
+                n = 9
+              )(Admissões)
+  )
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
