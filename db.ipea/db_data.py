c6a5c04cdd4b901e02b63f75125aebb7a3da9ca2
PhHeinzenFurb <phheinzen@furb.br>
2025-07-15 16:48:34 -0300

    Atualizado db_data.py
    Retirado o excesso de comentários

--- db.ipea/db_data.py
+++ db.ipea/db_data.py
@@ -19,6 +19,16 @@ banco = "ipea"
 # criando a egine de acesso ao PostgreSQL
 engine = create_engine(f"postgresql+psycopg2://{usuario}:{senha}@{host}:{porta}/{banco}")
 
+"""
+Função para o tratamento dos dados de Demissão e Admissão sem ajuste do IPEA-CAGED
+
+Códigos de referência: 
+    - ADMISNC = Admissões
+    - DESLIGNC = Demissões
+    
+Para gerar o df, utilziar o pacote ipeadatapy e a função timeseries()
+    - ipeadatapy.timeseries("ADMISNC", year=2020)
+"""
 def prep_AdmiDemisaso(df):
     df = df[(df['NIVNOME'] == "Municípios") & (df["VALUE (Pessoa)"] != 0)]
     df = df.rename(columns={
@@ -33,38 +43,10 @@ df_admissoes = ipeadatapy.timeseries("ADMISNC", year=2020)
 
 df_admissoes = prep_AdmiDemisaso(df_admissoes)
 
-# filtrando apenas os municipios
-#df_admissoes = df_admissoes[(df_admissoes['NIVNOME'] == "Municípios") & (df_admissoes["VALUE (Pessoa)"] != 0 )]
-
-# renomeando as colunas
-#df_admissoes = df_admissoes.rename(columns={
-#    "VALUE (Pessoa)": "value",
-#    "RAW DATE": "raw_date"
-#    })
-
-# colocando colunas em caixa baixa
-#df_admissoes.columns = [col.lower() for col in df_admissoes.columns]
-
-# criando df para os dados de demissoes
-#df_demissoes = ipeadatapy.timeseries("DESLIGNC")
-
-# filtrando apenas os municipios
-# = df_demissoes[(df_demissoes['NIVNOME'] == "Municípios") & (df_demissoes["VALUE (Pessoa)"] != 0 )]
-
-# renomeando as colunas
-#df_demissoes = df_demissoes.rename(columns={
-#    "VALUE (Pessoa)": "value",
-#    "RAW DATE": "raw_date"
-#    })
-
-# colocando colunas em caixa baixa
-#df_demissoes.columns = [col.lower() for col in df_demissoes.columns]
-
-
 
 # inserindo dados dentro das tabelas criadas
 #df_admissoes.to_sql("admissoesMunicipios", con=engine, if_exists="append", index=False)
-#df_demissoes.to_sql("demissoesMunicipios", con=engine, if_exists="append", index=False)
+
 
 
 
